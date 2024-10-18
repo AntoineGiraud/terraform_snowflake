533dfd0bae28e75a0bd37643d8f40dd7c295f713
AntoineGiraud <antoine.giraud@outlook.com>
2024-10-18 10:13:54 +0200

    feat: ajout runner dev vs prod

--- output.tf
+++ output.tf
@@ -1,12 +1,20 @@
 # ------------------------------------
 # loader keys
 # ------------------------------------
-output "cdc_runner_public_key" {
-  value = tls_private_key.key_cdc_runner.public_key_pem
+output "cdc_runner_public_key_prod" {
+  value = tls_private_key.key_cdc_runner_prod.public_key_pem
 }
 
-output "cdc_runner_private_key" {
-  value     = tls_private_key.key_cdc_runner.private_key_pem
+output "cdc_runner_private_key_prod" {
+  value     = tls_private_key.key_cdc_runner_prod.private_key_pem
+  sensitive = true
+}
+output "cdc_runner_public_key_dev" {
+  value = tls_private_key.key_cdc_runner_dev.public_key_pem
+}
+
+output "cdc_runner_private_key_dev" {
+  value     = tls_private_key.key_cdc_runner_dev.private_key_pem
   sensitive = true
 }
 
--- role_loader.tf
+++ role_loader.tf
@@ -39,32 +39,67 @@ resource "snowflake_grant_privileges_to_account_role" "wh_grant_loader" {
 // SECURITYADMIN & add usr to group
 // ---------------------------------------------
 
-resource "tls_private_key" "key_cdc_runner" {
+resource "tls_private_key" "key_cdc_runner_prod" {
   algorithm = "RSA"
   rsa_bits  = 2048
 }
 
-resource "snowflake_user" "cdc_runner" {
+resource "snowflake_user" "cdc_runner_prod" {
   provider          = snowflake.security_admin
-  name              = "CDC_RUNNER"
+  name              = "CDC_RUNNER_PROD"
   default_warehouse = snowflake_warehouse.wh_loader.name
   default_role      = snowflake_account_role.role_loader.name
   default_namespace = snowflake_database.db_bronze.name
-  rsa_public_key    = substr(tls_private_key.key_cdc_runner.public_key_pem, 27, 398)
+  rsa_public_key    = substr(tls_private_key.key_cdc_runner_prod.public_key_pem, 27, 398)
 }
 
-resource "snowflake_grant_privileges_to_account_role" "user_grant_loader" {
+resource "snowflake_grant_privileges_to_account_role" "user_grant_loader_prod" {
   provider          = snowflake.security_admin
   privileges        = ["MONITOR"]
   account_role_name = snowflake_account_role.role_loader.name
   on_account_object {
     object_type = "USER"
-    object_name = snowflake_user.cdc_runner.name
+    object_name = snowflake_user.cdc_runner_prod.name
   }
 }
 
-resource "snowflake_grant_account_role" "grants_usr_loader" {
+resource "snowflake_grant_account_role" "grants_usr_loader_prod" {
   provider  = snowflake.security_admin
   role_name = snowflake_account_role.role_loader.name
-  user_name = snowflake_user.cdc_runner.name
+  user_name = snowflake_user.cdc_runner_prod.name
+}
+
+
+// ---------------------------------------------
+// SECURITYADMIN & add usr to group
+// ---------------------------------------------
+
+resource "tls_private_key" "key_cdc_runner_dev" {
+  algorithm = "RSA"
+  rsa_bits  = 2048
+}
+
+resource "snowflake_user" "cdc_runner_dev" {
+  provider          = snowflake.security_admin
+  name              = "CDC_RUNNER_DEV"
+  default_warehouse = snowflake_warehouse.wh_loader.name
+  default_role      = snowflake_account_role.role_loader.name
+  default_namespace = snowflake_database.db_bronze.name
+  rsa_public_key    = substr(tls_private_key.key_cdc_runner_dev.public_key_pem, 27, 398)
+}
+
+resource "snowflake_grant_privileges_to_account_role" "user_grant_loader_dev" {
+  provider          = snowflake.security_admin
+  privileges        = ["MONITOR"]
+  account_role_name = snowflake_account_role.role_loader.name
+  on_account_object {
+    object_type = "USER"
+    object_name = snowflake_user.cdc_runner_dev.name
+  }
+}
+
+resource "snowflake_grant_account_role" "grants_usr_loader_dev" {
+  provider  = snowflake.security_admin
+  role_name = snowflake_account_role.role_loader.name
+  user_name = snowflake_user.cdc_runner_dev.name
 }
